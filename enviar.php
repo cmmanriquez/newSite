3692d06868c1fa41f8b155f266f06b859988584f
Camila Munioz <cm.manriquez@gmail.com>
2022-10-03 16:59:52 -0300

    nuevas animaciones

--- enviar.php
+++ enviar.php
@@ -1,16 +1,16 @@
 <?php
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
-$message = $_POST['mensaje'];
+$mensaje = $_POST['mensaje'];
 
-$header = 'De: ' . $mail . " \r\n";
+$header = 'De: ' . $email . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje = "Este mensaje fue enviado por  " . "$nombre" . ",\r\n";
+$mensaje .= "Su e-mail es: " . "$email" . " \r\n";
+$mensaje .= "Mensaje: " . "$_POST['mensaje']" . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'cm.manriquez@gmail.com';
