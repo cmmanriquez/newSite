5751befea3ae3fe09f9ec7b6fcbf24cb5dc0d9e0
Camila Munoz <cm.manriquez@gmail.com>
2023-03-03 14:34:22 -0300

    new changes

--- enviar.php
+++ enviar.php
@@ -13,10 +13,19 @@ $mensaje .= "Su e-mail es: " . "$email" . " \r\n";
 $mensaje .= "Mensaje: " . "$_POST['mensaje']" . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'cm.manriquez@gmail.com';
+$para = 'email de ustedes';
 $asunto = 'Mensaje desde la web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
 header("Location:ty.html");
 ?>
+
+<!-- formulario -->
+    <form id="contact" class="container__index-form" method="post" action="./enviar.php">
+            <input id="nombre" type="text" name="nombre" placeholder="nombre" required="required">
+            <input type="email" name="email" id="email" placeholder="email" required="required">
+            <label for="mensaje">MENSAJE</label>
+            <textarea name="mensaje" id="mensaje" cols="50" rows="4" style="resize:none;"></textarea>
+            <button class="btn_propio" name="enviar" type="submit">ENVIAR</button>
+    </form>
